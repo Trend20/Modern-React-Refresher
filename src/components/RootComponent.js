b83a0b4b5c1887c644bd96031bb2b4d081532bcd
Trend20 <enockomondi305@gmail.com>
2023-04-11 15:14:06 +0300

    a repo for modern react

---
+++ src/components/RootComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import UseCallback from '../hooks/UseCallback'
+
+const RootComponent = () => {
+  return (
+    <div>
+      <UseCallback />
+    </div>
+  )
+}
+
+export default RootComponent
---
+++ src/hooks/UseCallback.js
@@ -0,0 +1,12 @@
+import { useCallback } from "react";
+
+const UseCallback = () =>{
+  return (
+    <div>
+      <p>useCallback Hook</p>
+      <p>This hook lets you cache a function definition between re-renders.</p>
+    </div>
+  )
+}
+
+export default UseCallback;
